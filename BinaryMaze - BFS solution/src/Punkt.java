a00346016b379ad8644ee0c46215bdb2a3a1bda4
janzaczek <59251601+janzaczek@users.noreply.github.com>
2020-01-18 16:42:19 +0100

    Add files via upload

---
+++ BinaryMaze - BFS solution/src/Punkt.java
@@ -0,0 +1,13 @@
+public class Punkt {
+    int x,y;
+
+    public Punkt(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return "(" + x + "," + y + ")";
+    }
+}
